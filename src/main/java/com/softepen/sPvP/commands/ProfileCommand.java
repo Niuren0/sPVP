b5c34211ce230a233b8f1041c7919b66165d7e11
Niuren_ <nsmrkose@gmail.com>
2024-10-31 22:03:40 +0300

    fix profile command perm

--- src/main/java/com/softepen/sPvP/commands/ProfileCommand.java
+++ src/main/java/com/softepen/sPvP/commands/ProfileCommand.java
@@ -8,28 +8,30 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-import static com.softepen.sPvP.sPvP.messagesManager;
+import static com.softepen.sPvP.sPvP.*;
 
 public class ProfileCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
-        if (!(commandSender instanceof Player opener)) {
-            commandSender.sendMessage(messagesManager.getPrefixString("onlyPlayers"));
-            return true;
-        }
-
-        if (args.length == 0) {
-            new ProfileMenu(opener, opener);
-        } else {
-            Player player = Bukkit.getPlayer(args[0]);
-            if (player == null) {
-                opener.sendMessage(messagesManager.getPrefixString("cantFindPlayer"));
+        if (commandSender.hasPermission("spvp.commands.profile") || commandSender.hasPermission("spvp.commands.*") ||commandSender.hasPermission("spvp.*")) {
+            if (!(commandSender instanceof Player opener)) {
+                commandSender.sendMessage(messagesManager.getPrefixString("onlyPlayers"));
                 return true;
             }
 
-            new ProfileMenu(opener, player);
-        }
+            if (args.length == 0) {
+                new ProfileMenu(opener, opener);
+            } else {
+                Player player = Bukkit.getPlayer(args[0]);
+                if (player == null) {
+                    opener.sendMessage(messagesManager.getPrefixString("cantFindPlayer"));
+                    return true;
+                }
 
+                new ProfileMenu(opener, player);
+            }
+
+        } else commandSender.sendMessage(messagesManager.getPrefixString("noPerm"));
 
         return true;
     }
