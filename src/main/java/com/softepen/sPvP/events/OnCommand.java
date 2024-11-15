3f6aac1d07d354a0e357a9e60f4769b1d43f3ec8
Niuren_ <nsmrkose@gmail.com>
2024-11-15 19:15:59 +0300

    added whitelisted commands for freeze

--- src/main/java/com/softepen/sPvP/events/OnCommand.java
+++ src/main/java/com/softepen/sPvP/events/OnCommand.java
@@ -5,14 +5,21 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
-import static com.softepen.sPvP.sPvP.frozens;
-import static com.softepen.sPvP.sPvP.messagesManager;
+import java.util.Objects;
+
+import static com.softepen.sPvP.sPvP.*;
 
 public class OnCommand implements Listener {
     @EventHandler
     public void onCommandUse(PlayerCommandPreprocessEvent event) {
         Player player = event.getPlayer();
         if (frozens.containsKey(player)) {
+            String command = event.getMessage().split(" ")[0];
+
+            for (String wlCommand : configManager.getStringList("freeze.whitelistCommands")) {
+                if (Objects.equals("/" + wlCommand, command)) return;
+            }
+
             event.setCancelled(true);
             player.sendMessage(messagesManager.getPrefixString("frozenBy").replace("{staff}", frozens.get(player).getName()));
         }
